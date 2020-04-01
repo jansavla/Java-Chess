c4b44fbe49ec14c35301525f707101b8b3f88a2b
jansavla <62986882+jansavla@users.noreply.github.com>
2020-04-01 10:35:39 +0200

    Add files via upload

---
+++ firstClass.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.javachess;
+
+/**
+ *
+ * @author Fobosen
+ */
+public class firstClass {
+    public static void main(String[] args){
+        
+    }
+}
